2c7a6f9c424bbbcb3fe5d6c27cfb03d78da3f914
Blendi Spahiu <blend.sph98@gmail.com>
2022-08-20 02:40:56 +0200

    First commit

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+/* craco.config.js */
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@atoms": path.resolve(__dirname, "src/components/atoms/index"),
+      "@molecules": path.resolve(__dirname, "src/components/molecules/index"),
+      "@organisms": path.resolve(__dirname, "src/components/organisms/index"),
+      "@templates": path.resolve(__dirname, "src/components/templates/index"),
+      "@interfaces": path.resolve(__dirname, "src/interfaces/index"),
+      "@pages": path.resolve(__dirname, "src/pages/index"),
+      "@contexts": path.resolve(__dirname, "src/contexts/index"),
+      "@validators": path.resolve(__dirname, "src/validators/index"),
+      "@hooks": path.resolve(__dirname, "src/hooks/index"),
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export const App = (): ReactElement => {
+  return <div className="text-red-500">Test</div>;
+};
